f4d626880d39e667e7fbaa24a798fc290ba68d0e
Adam Andersson <erbennis@gmail.com>
2022-04-14 10:36:15 -0700

    Basic Item Interaction
    Item pick-up and placement

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    private Pickup currentHeld;
+    private Pickup currentNear;
+
     void Awake()
     {
-        
+        currentNear = null;
+        currentHeld = null;
     }
 
     void FixedUpdate()
@@ -19,6 +23,39 @@ public class PlayerInteraction : MonoBehaviour
 
     private void ChickenInteract()
     {
+        // pick up near object
+        if(currentHeld == null && currentNear != null)
+        {
+            Debug.Log("Picking up item");
+            currentHeld = currentNear;
+            currentHeld.gameObject.GetComponent<BoxCollider>().enabled = false;
+            currentHeld.transform.SetParent(this.transform);
+        }
+        // place held object
+        else if(currentHeld != null)
+        {
+            Debug.Log("Placing item");
+            currentHeld.transform.SetParent(null);
+            currentHeld.gameObject.GetComponent<BoxCollider>().enabled = true;
+            currentHeld = null;
+        }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Pickupable"))
+        {
+            Debug.Log("Item in range");
+            currentNear = other.gameObject.GetComponentInParent<Pickup>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pickupable"))
+        {
+            Debug.Log("Item not in range");
+            currentNear = null;
+        }
     }
 }
